7f4fc975d27d95af6fa79b7461ec3f4a9e98fd46
sanket kumar <sanket_@outlook.com>
2021-02-23 01:11:57 +0530

    update

--- Dsa/Tree.java
+++ Dsa/Tree.java
@@ -1,10 +1,12 @@
 package dsa;
+
 import java.util.Stack;
 import java.util.LinkedList;
 import java.util.Queue;
+
 class Tree {
     Node head;
-    int max=0;
+    int max = 0;
 
     static class Node {
         int data;
@@ -12,7 +14,8 @@ class Tree {
         Node right;
 
         Node(int data) {
-            this.data = data;//left and right are null as non-initialized variables are null in java but not in c++
+            this.data = data;// left and right are null as non-initialized variables are null in java but not
+                             // in c++
         }
     }
 
@@ -23,16 +26,17 @@ class Tree {
             inorder(head.right);
         }
     }
-    void preorder(Node head){
-        if(head!=null){
+
+    void preorder(Node head) {
+        if (head != null) {
             System.out.print(head.data + " ");
             preorder(head.left);
             preorder(head.right);
         }
     }
 
-    void postorder(Node head){
-        if(head!=null){
+    void postorder(Node head) {
+        if (head != null) {
             preorder(head.left);
             preorder(head.right);
             System.out.print(head.data + " ");
@@ -43,102 +47,97 @@ class Tree {
         if (head == null) {
             return 0;
         }
-        return 1+getSize(head.left)+getSize(head.right);
+        return 1 + getSize(head.left) + getSize(head.right);
     }
 
-
-
-
-    //  maximum  
-    void maximum(Node head){
-        if(head!=null){
+    // maximum
+    void maximum(Node head) {
+        if (head != null) {
             maximum(head.left);
-            if(head.data>this.max)max=head.data;
+            if (head.data > this.max)
+                max = head.data;
             maximum(head.right);
         }
-      
-        System.out.println("max till now: "+this.max );       
+
+        System.out.println("max till now: " + this.max);
     }
 
-
-    int max(Node head){
-        if(head==null)return Integer.MIN_VALUE;
+    int max(Node head) {
+        if (head == null)
+            return Integer.MIN_VALUE;
         else
-            return Math.max(head.data,Math.max(max(head.left),max(head.right))); 
+            return Math.max(head.data, Math.max(max(head.left), max(head.right)));
     }
-    /* recursive vs iterative
-        which method to use will depend on type of binary trree
-        - if tree is skewed then iterative method using a queue(level order) performs better then recursive
-    */
-    //
+    /*
+     * recursive vs iterative which method to use will depend on type of binary
+     * trree - if tree is skewed then iterative method using a queue(level order)
+     * performs better then recursive
+     */
 
+    void inorderWithoutRecursion(Node head) {
+        if (head == null)
+            return;
+        // look at the algorithm below(gfg) and try to code first
+        // https://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion/
 
-    void inorderWithoutRecursion(Node head){
-        if(head==null)return;
-        //look at the algorithm below(gfg) and try to code first
-        //https://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion/
+        Node current = head;
+        Stack<Node> s = new Stack<>();
 
-
-        
-        
-        
-        
-        
-        
-        Node current =head;
-        Stack<Node> s=new Stack<>();
-        
-        while(current!=null || s.size()>0){
+        while (current != null || s.size() > 0) {
             // first reach leftest node
-            while(current != null){
+            while (current != null) {
                 s.push(current);
-                current=current.left;
+                current = current.left;
             }
 
-            current=s.pop();
+            current = s.pop();
             System.out.println(current.data);
 
-            current=current.right;
+            current = current.right;
 
         }
-        
+
     }
 
-    void levelTraverse(Node head){
-       
-        Node current =head;
+    void levelTraverse(Node head) {
 
-        Queue<Node> q=new LinkedList<>();
-        
-        /*
-        ->> My Answer Code
+        Node current = head;
 
-         q.add(current);
-         while(q.peek() != null){
-            q.add(q.peek().left);
-            q.add(q.peek().right);
-          System.out.println(q.poll().data);
-        }
-        */
-
+        Queue<Node> q = new LinkedList<>();
 
         /*
+         * ->> My Answer Code
+         * 
+         * q.add(current); while(q.peek() != null){ q.add(q.peek().left);
+         * q.add(q.peek().right); System.out.println(q.poll().data); }
+         */
 
-        ->>Standard Answer Code
-
-        if(head==null)return;
-        q.add(current);
-        while(!q.isEmpty()){
-            current=q.poll();
-            System.out.println(current.data);
-
-            if(current.left!=null)q.add(current.left);
-            if(current.right!=null)q.add(current.right);
-
-        }
-
-        */
+        /*
+         * 
+         * ->>Standard Answer Code
+         * 
+         * if(head==null)return; q.add(current); while(!q.isEmpty()){ current=q.poll();
+         * System.out.println(current.data);
+         * 
+         * if(current.left!=null)q.add(current.left);
+         * if(current.right!=null)q.add(current.right);
+         * 
+         * }
+         * 
+         */
     }
+
+    // height of a tree
+
+    // we find height of left and right subtree recursively and get maximum of those
+    int height(Node head) {
+        if (head == null)
+            return 0;
+        else {
+            return (Math.max(height(head.left), height(head.right)) + 1);
+        }
+    }
+
 }
 
 class TreeDs {
@@ -156,15 +155,12 @@ class TreeDs {
         Tree.Node four = new Tree.Node(4);
         two.left = four;
         /*
-
-        tree structure  ->
-
-                 1
-              2       3
-          4
-
-        */
-
+         * 
+         * tree structure ->
+         * 
+         * 1 2 3 4
+         * 
+         */
 
         // headnode.inorder(headnode.head);
         // System.out.println();
@@ -174,9 +170,9 @@ class TreeDs {
         // System.out.println();
         // System.out.println(headnode.getSize(headnode.head));
 
-       // headnode.inorderWithoutRecursion(headnode.head);
-       //headnode.levelTraverse(headnode.head);
-      System.out.println(headnode.max(headnode.head)); 
+        // headnode.inorderWithoutRecursion(headnode.head);
+        // headnode.levelTraverse(headnode.head);
+        System.out.println(headnode.height(headnode.head));
 
     }
 }
---
+++ sorting/Sort.java
@@ -0,0 +1,9 @@
+package sorting;
+
+public class Sort{
+    public static void main(String[] args) {
+        int[] a={6,5,1,4,3,2};
+        Insertion a1=new Insertion(a);
+        
+    }
+}
